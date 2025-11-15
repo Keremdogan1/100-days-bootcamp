e778d6b60cc3c89c0b4cf324ce8dbdc84fc5c05b
Keremdogan1 <kerem.dogan111@gmail.com>
2025-11-15 16:05:51 +0300

    completed

---
+++ Day 1/task/task.py
@@ -0,0 +1,7 @@
+print ("Welcome to the band name creator.")
+
+city = input("What is the name of the city you grew up in? : ")
+
+pet = input("What is your pet's name? : ")
+
+print("Your band name is: ", city, pet)
